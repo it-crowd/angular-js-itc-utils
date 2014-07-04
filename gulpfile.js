176749294a4a3d8394e560d8389715e57b30628b
Bernard Labno <bernard.labno@itcrowd.pl>
2014-07-04 17:13:09 +0200

    Gulp and bower

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,41 @@
     var clean = require('gulp-clean');
     var concat = require('gulp-concat');
     var rename = require('gulp-rename');
+    var subtree = require('gulp-subtree');
 
-    gulp.task('dist', function ()
+    function dist()
     {
         var result = gulp.src('src/**/*.js');
-        result = result.pipe(jshint('.jshintrc'));
-        result = result.pipe(jshint.reporter('default'));
         result = result.pipe(concat('angular-itc-utils.js'));
-        result = result.pipe(uglify());
-        result = result.pipe(rename({suffix: '.min'}));
-        result = result.pipe(gulp.dest('dist'));
-        return result;
-    });
+        result.pipe(gulp.dest('dist'));
 
-    gulp.task('clean', function ()
+        result = gulp.src('src/**/*.js');
+        result = result.pipe(concat('angular-itc-utils.min.js'));
+        result = result.pipe(uglify());
+        result.pipe(gulp.dest('dist'));
+
+        result = gulp.src(['src/**/*.js', 'bower.json']);
+        result.pipe(gulp.dest('dist'));
+    }
+
+    function doClean()
     {
         return gulp.src(['dist', 'target'], {read: false}).pipe(clean());
-    });
+    }
+
+    function doJSHint()
+    {
+        return gulp.src('src/**/*.js').pipe(jshint('.jshintrc')).pipe(jshint.reporter('default'));
+    }
+
+    function doSubtree()
+    {
+        return gulp.src('dist').pipe(subtree({branch: 'bower'})).pipe(clean());
+    }
+
+    gulp.task('clean', doClean);
+    gulp.task('dist', ['clean'], dist);
+    gulp.task('jshint', doJSHint);
+    gulp.task('bower', doSubtree);
+    gulp.task('default', ['clean', 'jshint', 'dist']);
 })();
